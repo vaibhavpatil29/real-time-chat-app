c83681afe2117edbb80b246fef74c38a2f1bf1a9
vaibhavpatil29 <vaibhav292005v@gmail.com>
2025-08-05 14:40:18 +0530

    Update run.py with production configurations

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
+import eventlet
+eventlet.monkey_patch()
+from flask import Flask, render_template, request, redirect
+from flask_socketio import SocketIO
+
 from app import create_app, socketio
 
 app = create_app()
